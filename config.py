bfc9c98be6664c35674fc68fa373c543b3bf9193
Rainbow-Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2020-08-01 13:34:45 -0400

    update

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
-config_dict = {
-    'background_image': 'G:/university/MyAI project/pictures/3.jpg',
-    'background_places': '700, 0',
-    'eachline_character': '85'
-}
+﻿config_dict = {'background_image': 'G:/university/MyAI project/pictures/3.jpg', 'background_places': '700,0', 'eachline_character': '85'}
 path_enable_list = ['background_image']
