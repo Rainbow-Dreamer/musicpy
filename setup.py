a4d91cb912588da2716c4bcfc13b909dadfd16ae
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-02-03 01:40:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from setuptools import setup
 from os import path
 setup(
-  name = 'musicpy', 
-  packages = ['musicpy'],
-  package_data = {'musicpy': ['./*'],},
-  version = '1.68',  
-  license='bsd-2-clause',    
-  description = 'A music programming language to write music in syntax based on music theory, and make a piece of music simple in short sentences', 
-  author = 'Rainbow-Dreamer',
-  author_email = '1036889495@qq.com', 
-  url = 'https://github.com/Rainbow-Dreamer/musicpy.git',
-  download_url = 'https://github.com/Rainbow-Dreamer/musicpy/archive/1.68.tar.gz',
-  keywords = ['music language', 'use codes to write music', 'music language for AI'],
-  install_requires=[  
-          'mido',
-          'midiutil',
-          'pygame',
-          'pillow'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',  
-    'Intended Audience :: Developers', 
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
-  long_description=open('README.md',encoding='utf-8').read(),
-  long_description_content_type='text/markdown',
-  include_package_data=True
-)
+    name='musicpy',
+    packages=['musicpy'],
+    package_data={
+        'musicpy': ['./*'],
+    },
+    version='1.69',
+    license='bsd-2-clause',
+    description=
+    'Musicpy is a python domain-specific language designed to write music in very handy syntax for musicians. 这是一个可以让你用编程写音乐的python领域特定语言，可以让你用简洁的语法通过乐理知识写出优美的音乐。',
+    author='Rainbow-Dreamer',
+    author_email='1036889495@qq.com',
+    url='https://github.com/Rainbow-Dreamer/musicpy.git',
+    download_url=
+    'https://github.com/Rainbow-Dreamer/musicpy/archive/1.69.tar.gz',
+    keywords=[
+        'music language', 'use codes to write music', 'music language for AI'
+    ],
+    install_requires=['mido', 'midiutil', 'pygame', 'pillow'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True)
