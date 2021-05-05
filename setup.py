73e36432ae691dcce0f330d07a9ca3c9f003a2e3
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-05-06 02:32:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU Affero General Public License v3.0',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
