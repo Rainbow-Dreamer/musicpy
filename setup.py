eb354dacc9522ac0bcc0ce889f2e0bbfe3baa462
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-05-06 01:45:12 +0800

    update

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 from os import path
+
 setup(
     name='musicpy',
     packages=['musicpy'],
     package_data={
         'musicpy': ['./*'],
     },
-    version='1.97',
-    license='bsd-2-clause',
+    version='1.98',
+    license='AGPLv3',
     description=
     'Musicpy is a python domain-specific language designed to write music in very handy syntax for musicians. 这是一个可以让你用编程写音乐的python领域特定语言，可以让你用简洁的语法通过乐理知识写出优美的音乐。',
     author='Rainbow-Dreamer',
     author_email='1036889495@qq.com',
     url='https://github.com/Rainbow-Dreamer/musicpy.git',
     download_url=
-    'https://github.com/Rainbow-Dreamer/musicpy/archive/1.97.tar.gz',
+    'https://github.com/Rainbow-Dreamer/musicpy/archive/1.98.tar.gz',
     keywords=[
         'music language', 'use codes to write music', 'music language for AI'
     ],
@@ -25,7 +26,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU Affero General Public License v3.0',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
