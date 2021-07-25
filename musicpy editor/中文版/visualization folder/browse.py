49302e5cc7d7ffc725c59e6d1c6fd1818aa7e059
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-07-25 20:35:00 +0800

    update

--- musicpy editor/中文版/visualization folder/browse.py
+++ musicpy editor/中文版/visualization folder/browse.py
@@ -5,7 +5,16 @@ set_bpm = None
 
 def setup():
     global read_result
-    all_tracks = read(file_path, None, 'all', get_off_drums=get_off_drums)
+    all_tracks = read(file_path,
+                      None,
+                      mode='all',
+                      get_off_drums=get_off_drums,
+                      to_piece=True)
+    tempo_changes = all_tracks.get_tempo_changes()
+    all_tracks.clear_tempo()
+    all_tracks = [(all_tracks.tempo, all_tracks.tracks[i],
+                   all_tracks.start_times[i])
+                  for i in range(len(all_tracks.tracks))]
     if clear_pitch_bend:
         for each in all_tracks:
             each[1].clear_pitch_bend(value=0)
@@ -38,6 +47,7 @@ def setup():
         if i > 0:
             all_track_notes &= (current_track,
                                 current[2] - first_track_start_time)
+    all_track_notes += tempo_changes
     all_track_notes.normalize_tempo(tempo, start_time=first_track_start_time)
     if clear_all_pitch_bend:
         all_track_notes = all_track_notes.only_notes()
