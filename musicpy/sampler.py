58a71c63991a113dbff8bcd0ac9fe4757c5f1582
Rainbow Dreamer <2180502841@qq.com>
2021-10-13 12:43:57 +0800

    Update sampler.py

--- musicpy/sampler.py
+++ musicpy/sampler.py
@@ -23,102 +23,6 @@ import pickle
 
 os.chdir(file_path)
 
-default_notedict = {
-    'A0': 'A0',
-    'A#0': 'A#0',
-    'B0': 'B0',
-    'C1': 'C1',
-    'C#1': 'C#1',
-    'D1': 'D1',
-    'D#1': 'D#1',
-    'E1': 'E1',
-    'F1': 'F1',
-    'F#1': 'F#1',
-    'G1': 'G1',
-    'G#1': 'G#1',
-    'A1': 'A1',
-    'A#1': 'A#1',
-    'B1': 'B1',
-    'C2': 'C2',
-    'C#2': 'C#2',
-    'D2': 'D2',
-    'D#2': 'D#2',
-    'E2': 'E2',
-    'F2': 'F2',
-    'F#2': 'F#2',
-    'G2': 'G2',
-    'G#2': 'G#2',
-    'A2': 'A2',
-    'A#2': 'A#2',
-    'B2': 'B2',
-    'C3': 'C3',
-    'C#3': 'C#3',
-    'D3': 'D3',
-    'D#3': 'D#3',
-    'E3': 'E3',
-    'F3': 'F3',
-    'F#3': 'F#3',
-    'G3': 'G3',
-    'G#3': 'G#3',
-    'A3': 'A3',
-    'A#3': 'A#3',
-    'B3': 'B3',
-    'C4': 'C4',
-    'C#4': 'C#4',
-    'D4': 'D4',
-    'D#4': 'D#4',
-    'E4': 'E4',
-    'F4': 'F4',
-    'F#4': 'F#4',
-    'G4': 'G4',
-    'G#4': 'G#4',
-    'A4': 'A4',
-    'A#4': 'A#4',
-    'B4': 'B4',
-    'C5': 'C5',
-    'C#5': 'C#5',
-    'D5': 'D5',
-    'D#5': 'D#5',
-    'E5': 'E5',
-    'F5': 'F5',
-    'F#5': 'F#5',
-    'G5': 'G5',
-    'G#5': 'G#5',
-    'A5': 'A5',
-    'A#5': 'A#5',
-    'B5': 'B5',
-    'C6': 'C6',
-    'C#6': 'C#6',
-    'D6': 'D6',
-    'D#6': 'D#6',
-    'E6': 'E6',
-    'F6': 'F6',
-    'F#6': 'F#6',
-    'G6': 'G6',
-    'G#6': 'G#6',
-    'A6': 'A6',
-    'A#6': 'A#6',
-    'B6': 'B6',
-    'C7': 'C7',
-    'C#7': 'C#7',
-    'D7': 'D7',
-    'D#7': 'D#7',
-    'E7': 'E7',
-    'F7': 'F7',
-    'F#7': 'F#7',
-    'G7': 'G7',
-    'G#7': 'G#7',
-    'A7': 'A7',
-    'A#7': 'A#7',
-    'B7': 'B7',
-    'C8': 'C8'
-}
-
-pygame.quit()
-pygame.init()
-pygame.mixer.init(44100, -16, 2, 4096)
-pygame.mixer.set_num_channels(1000)
-
 
 class esi:
     def __init__(self,
@@ -1299,18 +1203,6 @@ def adsr_func(sound, attack, decay, sustain, release):
     return sound
 
 
-reverse = effect(lambda s: s.reverse(), 'reverse')
-offset = effect(lambda s, bar, bpm: s[bar_to_real_time(bar, bpm, 1):],
-                'offset',
-                unknown_args={'bpm': None})
-fade_in = effect(lambda s, duration: s.fade_in(duration), 'fade in')
-fade_out = effect(lambda s, duration: s.fade_out(duration), 'fade out')
-fade = effect(
-    lambda s, duration1, duration2=0: s.fade_in(duration1).fade_out(duration2),
-    'fade')
-adsr = effect(adsr_func, 'adsr')
-
-
 def sine(freq=440, duration=1000, volume=0):
     if type(freq) in [str, note]:
         freq = get_freq(freq)
@@ -1472,3 +1364,111 @@ def load_esi(file_path, convert=True):
         }
         current_esi.samples = sound_files
     return current_esi
+
+
+default_notedict = {
+    'A0': 'A0',
+    'A#0': 'A#0',
+    'B0': 'B0',
+    'C1': 'C1',
+    'C#1': 'C#1',
+    'D1': 'D1',
+    'D#1': 'D#1',
+    'E1': 'E1',
+    'F1': 'F1',
+    'F#1': 'F#1',
+    'G1': 'G1',
+    'G#1': 'G#1',
+    'A1': 'A1',
+    'A#1': 'A#1',
+    'B1': 'B1',
+    'C2': 'C2',
+    'C#2': 'C#2',
+    'D2': 'D2',
+    'D#2': 'D#2',
+    'E2': 'E2',
+    'F2': 'F2',
+    'F#2': 'F#2',
+    'G2': 'G2',
+    'G#2': 'G#2',
+    'A2': 'A2',
+    'A#2': 'A#2',
+    'B2': 'B2',
+    'C3': 'C3',
+    'C#3': 'C#3',
+    'D3': 'D3',
+    'D#3': 'D#3',
+    'E3': 'E3',
+    'F3': 'F3',
+    'F#3': 'F#3',
+    'G3': 'G3',
+    'G#3': 'G#3',
+    'A3': 'A3',
+    'A#3': 'A#3',
+    'B3': 'B3',
+    'C4': 'C4',
+    'C#4': 'C#4',
+    'D4': 'D4',
+    'D#4': 'D#4',
+    'E4': 'E4',
+    'F4': 'F4',
+    'F#4': 'F#4',
+    'G4': 'G4',
+    'G#4': 'G#4',
+    'A4': 'A4',
+    'A#4': 'A#4',
+    'B4': 'B4',
+    'C5': 'C5',
+    'C#5': 'C#5',
+    'D5': 'D5',
+    'D#5': 'D#5',
+    'E5': 'E5',
+    'F5': 'F5',
+    'F#5': 'F#5',
+    'G5': 'G5',
+    'G#5': 'G#5',
+    'A5': 'A5',
+    'A#5': 'A#5',
+    'B5': 'B5',
+    'C6': 'C6',
+    'C#6': 'C#6',
+    'D6': 'D6',
+    'D#6': 'D#6',
+    'E6': 'E6',
+    'F6': 'F6',
+    'F#6': 'F#6',
+    'G6': 'G6',
+    'G#6': 'G#6',
+    'A6': 'A6',
+    'A#6': 'A#6',
+    'B6': 'B6',
+    'C7': 'C7',
+    'C#7': 'C#7',
+    'D7': 'D7',
+    'D#7': 'D#7',
+    'E7': 'E7',
+    'F7': 'F7',
+    'F#7': 'F#7',
+    'G7': 'G7',
+    'G#7': 'G#7',
+    'A7': 'A7',
+    'A#7': 'A#7',
+    'B7': 'B7',
+    'C8': 'C8'
+}
+
+reverse = effect(lambda s: s.reverse(), 'reverse')
+offset = effect(lambda s, bar, bpm: s[bar_to_real_time(bar, bpm, 1):],
+                'offset',
+                unknown_args={'bpm': None})
+fade_in = effect(lambda s, duration: s.fade_in(duration), 'fade in')
+fade_out = effect(lambda s, duration: s.fade_out(duration), 'fade out')
+fade = effect(
+    lambda s, duration1, duration2=0: s.fade_in(duration1).fade_out(duration2),
+    'fade')
+adsr = effect(adsr_func, 'adsr')
+
+pygame.quit()
+pygame.init()
+pygame.mixer.init(44100, -16, 2, 4096)
+pygame.mixer.set_num_channels(1000)
