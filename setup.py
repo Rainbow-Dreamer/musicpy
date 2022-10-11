9af0a5d6dc5beb1a165d26c1b749c4793411fde1
Rainbow Dreamer <2180502841@qq.com>
2022-10-11 10:26:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='musicpy',
     packages=find_packages(),
     package_data={'musicpy': ['./*']},
-    version='5.51',
+    version='5.52',
     license='LGPLv2.1',
     description=
     'Musicpy is a music programming language in Python designed to write music in very handy syntax through music theory and algorithms.',
@@ -12,12 +12,12 @@ setup(
     author_email='1036889495@qq.com',
     url='https://github.com/Rainbow-Dreamer/musicpy.git',
     download_url=
-    'https://github.com/Rainbow-Dreamer/musicpy/archive/5.51.tar.gz',
+    'https://github.com/Rainbow-Dreamer/musicpy/archive/5.52.tar.gz',
     keywords=[
         'music language', 'use codes to write music', 'music language for AI'
     ],
     install_requires=[
-        'git+https://github.com/Rainbow-Dreamer/mido.git', 'pygame'
+        'mido @ git+https://github.com/Rainbow-Dreamer/mido.git', 'pygame'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
