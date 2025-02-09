1629956e512a9509466a8746589d2e2a7662e08d
I Love Study <1450069615@qq.com>
2025-02-10 02:19:24 +0800

    Add extras_require `[musicxml]`

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires=['mido-fix', 'pygame-ce', 'dataclasses'],
     extras_require={
         "daw": ["sf2_loader", "pedalboard", "scipy", "numpy"],
+        "musicxml": ["partitura"],
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
